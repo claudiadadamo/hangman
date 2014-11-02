789238c63e76658f3804507698126a112f92272c
Claudia D'Adamo <claudiadadamo@gmail.com>
2014-11-02 18:49:50 -0500

    added in check for when they choose 10 wrong letters

--- hangman_site/hangman/views.py
+++ hangman_site/hangman/views.py
@@ -32,30 +32,57 @@ def get_guess(request):
 		if form.is_valid():
 			# logipc - save to database 
 			print "hi"
+			wins = 0
+			losses = 0
 			new_guess = request.POST.get("guess")
 			# if new_guess in 
 			print new_guess
 			current_game = Game.objects.get(current=True)
 			print "wrong", current_game.wrong_guesses
 			print "word", current_game.word
+			print "current: ", current_game.current_state
+
 			print "prev guessed", current_game.guessed_letters
 			
+			
 			if current_game.wrong_guesses == 10:
-				current_game.status = "game over, you lose!"
-			if current_game.current_state == current_game.word:
-				current_game.status = "you win! now to play another game..."
+				current_game.status = "game over, you lose! new game!"
+				losses += 1
 				current_game.current = False
+				current_game.save()
+				random_word = Game.objects.get(pk=1)
+				random_word.current = True
+				random_word.save()
+				return HttpResponseRedirect('/hangman/index.html', {'form': form, 
+							'word':random_word, 
+							'status':random_word.status, 
+							'wins':wins, 
+							'losses':losses})
+
+			elif current_game.current_state == current_game.word:
+				current_game.status = "you win! now to play another game..."
+				wins +=1
+				current_game.current = False
+				current_game.save()
 				# CHANGE THIS LOGIC TO RANDOMLY CHOOSE A NEW WORD!!!
 				random_word = Game.objects.get(pk=1)
 				random_word.current = True
 				random_word.save()
+				return HttpResponseRedirect('/hangman/index.html', {'form': form, 
+							'word':random_word, 
+							'status':random_word.status, 
+							'wins':wins, 
+							'losses':losses})
 			else:
 				if new_guess in current_game.guessed_letters:
+					# if they already guessed that letter
 					current_game.status = "you already guessed that letter, guess again."
 					current_game.save()
 					return HttpResponseRedirect('/hangman/index.html', {'form': form})
 				else:
+					# if they guess a letter that wasn't previously guessed
 					if new_guess in current_game.word:
+						# of the letter is in the word
 						current_game.status = "nice, that letter was found. guess again."
 						char_list = list(current_game.current_state)
 						for m in re.finditer(new_guess,current_game.word):
@@ -65,9 +92,14 @@ def get_guess(request):
 						current_game.guessed_letters = current_game.guessed_letters+new_guess
 						current_game.save()
 						print current_game.current_state
-						return HttpResponseRedirect('/hangman/index.html', {'form': form, 'word':current_game, 'status':current_game.status})
+						return HttpResponseRedirect('/hangman/index.html', {'form': form, 
+							'word':current_game, 
+							'status':current_game.status, 
+							'wins':wins, 
+							'losses':losses})
 
 					else:
+						# if they guess a word that is not in the string
 						current_game.status = "not found, guess again"
 						current_game.wrong_guesses += 1
 						current_game.guessed_letters = current_game.guessed_letters+new_guess
