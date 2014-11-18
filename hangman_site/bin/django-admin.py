77172581eb0d23eea5592b6d50ffdf0dfb38ce9c
Claudia D'Adamo <claudiadadamo@gmail.com>
2014-11-17 19:16:16 -0500

    fixed error on linux

---
+++ hangman_site/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/DAdamo/Desktop/ActiveRepos/Hangman_Project/hangman_site/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
